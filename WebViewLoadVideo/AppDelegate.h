6abac044023706b95661c1e8fcf499a41f226cc4
liwangwnag123 <2317790024@qq.com>
2017-04-24 14:54:22 +0800

    Initial Commit

---
+++ WebViewLoadVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewLoadVideo
+//
+//  Created by 王力 on 2017/4/24.
+//  Copyright © 2017年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
